bacfdc6cc0f218282948e3bdb69df68e49e6a298
gowthamnandha <63248101+gowthamnandha@users.noreply.github.com>
2024-07-01 22:09:28 +0530

    updated the database to db.sqlite3
    updated it because i do not have mysql on my ec2

--- ecom/settings.py
+++ ecom/settings.py
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.mysql',
-		'NAME': 'django_react',
-		'USER': 'root',
-		'PASSWORD': '8310299076',
-		'HOST':'localhost',
-		'PORT':'3306',
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.mysql',
+# 		'NAME': 'django_react',
+# 		'USER': 'root',
+# 		'PASSWORD': '8310299076',
+# 		'HOST':'localhost',
+# 		'PORT':'3306',
+# 	}
+# }
 
 
 
